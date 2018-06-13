e7ab187503d0daad8519643e1af1d5092f678fbb
RenHongshuang <848057803@qq.com>
2018-06-14 00:43:33 +0800

    init the project

---
+++ app/src/main/java/com/samsung/android/spay/ui/SpayHomeActivity.java
@@ -0,0 +1,15 @@
+package com.samsung.android.spay.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.samsung.android.spay.R;
+
+public class SpayHomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spay_home);
+    }
+}
