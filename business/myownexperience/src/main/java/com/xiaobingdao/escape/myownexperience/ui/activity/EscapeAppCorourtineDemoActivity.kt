40bd462ba0ebf9813ae4614020209e48a4b65969
HongShuang.Ren <HongShuang.Ren@homecredit.cn>
2019-12-23 14:25:18 +0800

    add Coroutine module add res

---
+++ business/myownexperience/src/main/java/com/xiaobingdao/escape/myownexperience/ui/activity/EscapeAppCorourtineDemoActivity.kt
@@ -0,0 +1,16 @@
+package com.xiaobingdao.escape.myownexperience.ui.activity
+
+import android.os.Bundle
+import com.xiaobingdao.escape.base.ui.BaseActivity
+import com.xiaobingdao.escape.myownexperience.R
+
+/**
+ * Created by hs.ren on 2019/12/23.
+ */
+
+class EscapeAppCorourtineDemoActivity :BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cotourine_demo_layout)
+    }
+}
