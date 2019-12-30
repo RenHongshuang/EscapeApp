770b1e9d6b5ce9c094d05bc386c6d9b0c281f29f
RenHongshuang <848057803@qq.com>
2019-12-30 22:34:49 +0800

    add viewModelExt with coroutine functions

--- base/src/main/java/com/xiaobingdao/escape/base/ViewModelExt.kt
+++ base/src/main/java/com/xiaobingdao/escape/base/ViewModelExt.kt
@@ -1,16 +1,14 @@
 package com.xiaobingdao.escape.base
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelStore
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.CoroutineExceptionHandler
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 fun ViewModel.launch(
-    block: suspend CoroutineScope.() -> Unit,
-    onError: (e: Throwable) -> Unit = {},
-    onComplete: () -> Unit = {}
+        block: suspend CoroutineScope.() -> Unit,
+        onError: (e: Throwable) -> Unit = {},
+        onComplete: () -> Unit = {}
 ) {
     viewModelScope.launch(CoroutineExceptionHandler { _, e -> onError(e) }) {
         try {
@@ -19,6 +17,44 @@ fun ViewModel.launch(
             onComplete()
         }
     }
+}
+
+fun ViewModel.launch(
+        tryBlock: suspend CoroutineScope.() -> Unit,
+        catchBlock: suspend CoroutineScope.(Throwable) -> Unit,
+        finallyBlock: suspend CoroutineScope.() -> Unit,
+        handleCancellationExceptionManually: Boolean = false) {
+    viewModelScope.launch {
+        coroutineScope {
+            try {
+                tryBlock()
+            } catch (e: Throwable) {
+                if (e !is CancellationException || handleCancellationExceptionManually) {
+                    catchBlock(e)
+                } else {
+                    throw e
+                }
+            } finally {
+                finallyBlock()
+            }
+
+        }
+    }
+    
+    fun <T> ViewModel.launchOnIO(block: suspend CoroutineScope.() -> T) {
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                block
+            }
+        }
+
+    }
+
+    fun ViewModel.launchOnUI(block: suspend CoroutineScope.() -> Unit) {
+        viewModelScope.launch { block() }
+    }
+
+
 }
 
 
